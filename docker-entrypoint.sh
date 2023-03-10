8a3a1926116b3be5123a3951fb3708515d168f1d
Nivesh Mittapally <mittapally.nivesh@gmail.com>
2023-03-10 16:37:43 +0530

    added seed in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,7 @@ python manage.py createcachetable
 python manage.py migrate        # Apply database migrations
 python manage.py collectstatic --clear --noinput # clearstatic files
 python manage.py collectstatic --noinput  # collect static files
+python manage.py seed
 
 # directory for gunicorn logs and django app logs
 mkdir -p /ebs/logs
